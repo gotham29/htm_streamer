ace7b72908d762352ae7ebe9a031c892cd8c35cc
Sam Heiserman <sheiser1@binghamton.edu>
2022-10-20 13:41:07 -0700

    added -- run_nab() to driver.py

--- driver.py
+++ driver.py
@@ -21,8 +21,8 @@ def print_config(config, indents=2):
                 print(f"{indent}    = {v_}")
 
 
-def get_labels(ds_name):
-    with open(LABELS_PATH, 'r') as f:
+def get_labels(ds_name, path_labels):
+    with open(path_labels, 'r') as f:
         tags = json.loads(f.read())
 
     try:
@@ -35,9 +35,9 @@ def get_labels(ds_name):
     return anom_positions
 
 
-def get_data(ds_name):
+def get_data(ds_name, path_data):
     ds_file = None
-    for root, dirs, files in os.walk(DATA_PATH):
+    for root, dirs, files in os.walk(path_data):
         for f in files:
             if ds_name in f:
                 if ds_file:
@@ -51,9 +51,9 @@ def get_data(ds_name):
     return pd.read_csv(ds_file)
 
 
-def merge_labels_into_data(ds_name):
-    df = get_data(ds_name)
-    tags = get_labels(ds_name)
+def merge_labels_into_data(ds_name, path_data, path_labels):
+    df = get_data(ds_name, path_data)
+    tags = get_labels(ds_name, path_labels)
 
     df['label'] = 0
     df['anomaly_score'] = None
@@ -77,55 +77,56 @@ def get_args():
     return parser.parse_args()
 
 
-if __name__ == '__main__':
-
-    # Get args
-    args = get_args()
-    DATASET_COUNT = 60
-    PROBATION_PERCENT = 0.1
-    dir_htm = args.dir_htm  # "/Users/samheiserman/Desktop/repos/htm_streamer"
-    dir_nab = args.dir_nab  # "/Users/samheiserman/Desktop/repos/NAB"
-
-    CONFIG_PATH = os.path.join(dir_htm, 'data', 'config--nab.yaml')
-    CONFIG_DEF_PATH = os.path.join(dir_htm, 'data', 'config--default.yaml')
-    RESULTS_DIR = os.path.join(dir_nab, 'results', 'htmStreamer')
-    DATA_PATH = os.path.join(dir_nab, 'data')
-    LABELS_PATH = os.path.join(dir_nab, 'labels', 'combined_windows.json')
+def run_nab(dir_htm, dir_nab, dataset_count, probation_proportion):
+    path_config = os.path.join(dir_htm, 'data', 'config--nab.yaml')
+    path_config_def = os.path.join(dir_htm, 'data', 'config--default.yaml')
+    dir_results = os.path.join(dir_nab, 'results', 'htmStreamer')
+    path_data = os.path.join(dir_nab, 'data')
+    path_labels = os.path.join(dir_nab, 'labels', 'combined_windows.json')
 
     # Load config & data
-    config = load_config(CONFIG_PATH)
-    config_def = load_config(CONFIG_DEF_PATH)
+    config = load_config(path_config)
+    config_def = load_config(path_config_def)
     print_config(config)
     print_config(config_def)
 
     predictive_features = ['timestamp', 'value']
 
     with tqdm.tqdm(total=58, desc="\n\nRunning NAB") as p_bar:
-        for root, dirs, files in os.walk(DATA_PATH):
-            for f in files:
-                if f.endswith('.csv'):
-                    if p_bar.n >= DATASET_COUNT:
-                        exit(0)
-                    exp_name = os.path.splitext(f)[0]
-                    _, exp_folder = os.path.split(root)
-                    exp_folder_dir = os.path.join(RESULTS_DIR, exp_folder)
-                    os.makedirs(os.path.join(exp_folder_dir), exist_ok=True)
-                    results_path = os.path.join(exp_folder_dir, f"htmStreamer_{f}")
-                    data = merge_labels_into_data(exp_name)
-                    pred_data = data[predictive_features]
-                    # Train
-                    print(f"\n\n{f}")
-                    config_def['models_params']['alikl']['probationaryPeriod'] = int(data.shape[0]*PROBATION_PERCENT)
-                    features_models, features_outputs = run_batch(cfg=config,
-                                                                  cfg_default=config_def,
-                                                                  config_path=None,
-                                                                  config_default_path=None,
-                                                                  learn=True,
-                                                                  data=pred_data,
-                                                                  iter_print=999999,
-                                                                  features_models={})
-                    # Collect results
-                    data['anomaly_score'] = np.array(
-                        features_outputs[f'megamodel_features={len(predictive_features)}']['anomaly_likelihood'])
-                    data.to_csv(results_path)
-                    p_bar.update(1)
+        for root, dirs, files in os.walk(path_data):
+            files_csv = [f for f in files if f.endswith('.csv')]
+            for f in files_csv:
+                # if f.endswith('.csv'):
+                if p_bar.n >= dataset_count:
+                    exit(0)
+                exp_name = os.path.splitext(f)[0]
+                _, exp_folder = os.path.split(root)
+                exp_folder_dir = os.path.join(dir_results, exp_folder)
+                os.makedirs(os.path.join(exp_folder_dir), exist_ok=True)
+                results_path = os.path.join(exp_folder_dir, f"htmStreamer_{f}")
+                data = merge_labels_into_data(exp_name, path_data, path_labels)
+                pred_data = data[predictive_features]
+                # Train
+                print(f"\n\n{f}")
+                config_def['models_params']['alikl']['probationaryPeriod'] = int(data.shape[0] * probation_proportion)
+                features_models, features_outputs = run_batch(cfg=config,
+                                                              cfg_default=config_def,
+                                                              config_path=None,
+                                                              config_default_path=None,
+                                                              learn=True,
+                                                              data=pred_data,
+                                                              iter_print=999999,
+                                                              features_models={})
+                # Collect results
+                data['anomaly_score'] = np.array(
+                    features_outputs[f'megamodel_features={len(predictive_features)}']['anomaly_likelihood'])
+                data['raw_score'] = np.array(
+                    features_outputs[f'megamodel_features={len(predictive_features)}']['anomaly_score'])
+                data.to_csv(results_path)
+                p_bar.update(1)
+
+
+if __name__ == '__main__':
+    # Get args
+    args = get_args()
+    run_nab(args.dir_htm, args.dir_nab, dataset_count=60, probation_proportion=0.1)
